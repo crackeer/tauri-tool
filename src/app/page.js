77ad0d0a4f868456e394282ab4e15e44e12304c7
liuhu016 <liuhu016@realsee.com>
2024-03-26 10:36:06 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,190 +1,209 @@
 'use client'
-import React from 'react'
-import { Card, Avatar, Link, Typography, Space, Row, Input, Button, Message, Radio, Divider, Modal } from '@arco-design/web-react';
-import api from '@/util/api'
-import cache from '@/util/cache'
+import React from 'react';
+import 'jsoneditor/dist/jsoneditor.css';
+import { Button, Modal, List, Link, Space, Message, Popconfirm, Input, Drawer } from '@arco-design/web-react';
+import { IconSave, IconImport, IconFire, IconAlignCenter, IconRefresh, IconClockCircle, IconDown, IconShrink } from '@arco-design/web-react/icon';
+import { save, open } from '@tauri-apps/api/dialog';
 import JSONEditor from '@/component/JSONEditor';
-const RadioGroup = Radio.Group;
-const cacheKey = 'github-config'
-async function getConfig() {
-    let result = await cache.getJSON(cacheKey)
-    return result
+import invoke from '@/util/invoke'
+import common from '@/util/common'
+import cache from '@/util/cache'
+import jsonToGo from '@/util/json-to-go';
+import dayjs from 'dayjs'
+import ClickToCopy from '@/component/ClickToCopy';
+import { writeText } from '@tauri-apps/api/clipboard';
+
+const cacheKey = 'json-create@'
+const cacheKeyList = 'json-create-list'
+const setJSONCache = async (name, value) => {
+    await cache.setJSON(cacheKey + name, value)
+    addJSONCacheKey(name)
 }
-async function cacheSetConfig(data) {
-    let result = await cache.setJSON(cacheKey, data)
-    return result
-}
-
-async function getBookmarks(token, gist_id) {
-    let detail = await api.getGithubGist(token, gist_id)
-    let fileNames = Object.keys(detail.files)
-    let list = []
-    for (var i in fileNames) {
-
-        if (detail.files[fileNames[i]].language == 'JSON') {
-            console.log(i, fileNames[i], detail.files[fileNames[i]])
-            let result = await api.getGithubGistFile(detail.files[fileNames[i]].raw_url)
-            if (isBookmarkData(result)) {
-                list.push({
-                    'classify': fileNames[i],
-                    'list': result
-                })
-            }
-        }
-    }
-
-    return list
+const getJSONCache = async (name) => {
+    return await cache.getJSON(cacheKey + name)
 }
 
-function isBookmarkData(jsonData) {
-    return jsonData.length != undefined && jsonData[0].url != undefined
+const addJSONCacheKey = async (name) => {
+    let list = await getJSONCacheList() || []
+    list.push(name)
+    if (list.length > 20) {
+        list = list.slice(0, 20)
+    }
+    let object = {}
+    for (var i in list) {
+        object[list[i]] = true
+    }
+    return await cache.setJSON(cacheKeyList, Object.keys(object))
+}
+const getJSONCacheList = async () => {
+    return await cache.getJSON(cacheKeyList)
 }
 
-
-export default function App() {
-    var editor = null;
-    const [ready, setReady] = React.useState(false)
-    const [config, setConfig] = React.useState({ 'token': '', 'gist_id': '' })
-    const [visible, setVisible] = React.useState(false)
-    const [gistID, setGistID] = React.useState('')
-    const [tmpGistID, setTmpGistID] = React.useState('')
-    const [loading, setLoading] = React.useState(false)
-    const [token, setToken] = React.useState('')
-    const [gistList, setGistList] = React.useState([])
-    const [bookmark, setBookmarkData] = React.useState([])
-    const [open, setOpen] = React.useState(false)
-    const [editClassify, setEditClassify] = React.useState('')
-    async function initSetting() {
-        let config = await getConfig()
-        if (config == false || config == null || config.token == undefined) {
-            setReady(true)
-            return
-        }
-        setConfig(config)
-        setToken(config.token)
-        setBookmarks(config)
-        setReady(true)
-    }
-    async function finishSet() {
-        setConfig({
-            token: token,
-            gist_id: tmpGistID,
-        })
-        cacheSetConfig({
-            token: token,
-            gist_id: tmpGistID,
-        })
-        setBookmarks({
-            token: token,
-            gist_id: tmpGistID,
-        })
-    }
-    async function setBookmarks(input) {
-        let data = await getBookmarks(input.token, input.gist_id)
-        setBookmarkData(data)
-    }
-
-    async function getGists() {
-        try {
-            setLoading(true)
-            let result = await api.getGithubGists(token)
-            setGistList(result)
-        } catch (e) {
-            Message.error(e.message)
-        }
-        setLoading(false)
-
-    }
-    React.useEffect(() => {
-        initSetting()
-    }, [])
-
-    const doEditJSON = async (name) => {
-        setOpen(true)
-        setEditClassify(name)
-        for (var i in bookmark) {
-            if (bookmark[i].classify === name) {
-                if(editor != null) {
-                    editor.set(bookmark[i].list)
-                } else {
-                    setTimeout(() => {
-                        editor.set(bookmark[i].list)
-                    }, 600)
-                }
-                return
-            }
+class App extends React.Component {
+    editor = null;
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewHeight: 200,
+            convert: '',
+            convertTitle: '',
+            visible: false,
+            tmpName: '',
+            cacheVisible: false,
+            cacheNames: [],
         }
     }
-
-    const doUpdate = async () => {
-        let result = api.updateGithubGistFile(config.token, config.gist_id, editClassify, JSON.stringify(editor.get()))
-        console.log(result)
-    }
-
-    const clear = async function () {
-        cacheSetConfig({
-            token : '',
-            gist_id : '',
-        })
-        Message.info('success')
-    }
-
-    if (!ready) {
-        return '获取数据中'
-    }
-
-    return <div id="app">
-        <p>
-            <Button onClick={clear}>Clear</Button>
-        </p>
-        {
-            (config.gist_id.length < 1) ? <Card title='选择gists'>
-                <div>
-                    Github Token：<Input.TextArea value={token} onChange={setToken} style={{ width: '60%' }} placeholder="token"></Input.TextArea>
-                    <Button onClick={getGists} type='outline' style={{ marginLeft: 8 }} loading={loading}>查询gists</Button>
-                </div>
-                <Divider orientation='left'>Gists列表</Divider>
-                <RadioGroup direction='vertical' value={tmpGistID} onChange={setTmpGistID}>
-                    {
-                        gistList.map(item => {
-                            return <Radio value={item.id}>{item.description}</Radio>
-                        })
-                    }
-                </RadioGroup>
-                {
-                    tmpGistID.length > 0 ? <p>
-                        <Button onClick={finishSet} type='text' style={{ marginLeft: 8 }} loading={loading}>确认选择该gists作为书签保存位置？</Button>
-                    </p> : null
-                }
-            </Card> : null
-        }
-        {
-            bookmark.map(item => {
-                return <Card title={<>
-                    {item.classify} <Button type='text' onClick={doEditJSON.bind(this, item.classify)}>修改</Button>
-                </>} style={{ margin: '8px 0' }}>
-                    <Space size={'large'}>
-                        {item.list.map(tmp => {
-                            return <Link href={tmp.url} target='_blank' key={tmp.url}>{tmp.name || tmp.url}</Link>
-                        })}
-                    </Space>
-                </Card>
+    async componentDidMount() {
+        this.setState({ viewHeight: common.getViewHeight() })
+        window.onresize = () => {
+            this.setState({
+                viewHeight: common.getViewHeight(),
             })
         }
-        <Modal
-            title={editClassify}
-            alignCenter={false}
-            visible={open}
-            style={{ width: '60%', top: '100' }}
-            autoFocus={false}
-            focusLock={true}
-            onCancel={() => { setOpen(false) }}
-            onOk={doUpdate}
-        >
-            <JSONEditor height={'300px'} ref={(ele) => {
-                editor = ele
-            }} />
-        </Modal>
-    </div>
+    }
+     onJSONEditorReady = async (ele) => {
+        this.editor = ele
+        let value = await cache.getJSON("JSON.cache")
+        this.editor.set(value || {})
+    }
+    validateJSON = async (value) => {
+        cache.setJSON("JSON.cache", value)
+    }
+    saveJSON = async () => {
+        let file = await save({
+            filters: [{
+                name: "unknown",
+                extensions: ['json']
+            }]
+        });
+        if (file == null) return;
+        await invoke.writeFile(file, JSON.stringify(this.editor.get()))
+        Message.success('保存成功')
+    }
+    saveJSON2Cache = async () => {
+        setJSONCache(this.state.tmpName, this.editor.get())
+        Message.success('保存成功')
+    }
+    loadCacheJSON = async () => {
+        let list = await getJSONCacheList()
+        this.setState({ cacheNames: list, cacheVisible: true })
+    }
 
+    loadJSON = async () => {
+        let file = await open({
+            multipart: false,
+            filters: [{
+                name: '',
+                extensions: ['json']
+            }]
+        });
+        if (file == null) return;
+        let content = await invoke.readFile(file)
+        try {
+            let json = JSON.parse(content);
+            this.editor.set(json)
+        } catch (e) {
+        }
+    }
+    toGoStruct = () => {
+        let result = jsonToGo(JSON.stringify(this.editor.get()), null, null, false)
+        this.setState({
+            convert: result.go,
+            convertTitle: 'Go结构体',
+            visible: true,
+        })
+    }
+    toString = () => {
+        this.setState({
+            convert: JSON.stringify(JSON.stringify(this.editor.get())),
+            convertTitle: '序列化结果',
+            visible: true,
+        })
+    }
+    clearJSON = () => {
+        this.editor.set({})
+    }
+    importCacheJSON = async (name) => {
+        let data = await getJSONCache(name)
+        this.editor.set(data)
+        this.setState({ cacheVisible: false }, () => {
+            Message.info('导入成功')
+        })
+    }
+    copyCacheJSON = async (name) => {
+        let data = await getJSONCache(name)
+        writeText(JSON.stringify(data)).then(() => {
+            Message.info('复制成功')
+        })
+    }
+
+    render() {
+
+        return <div>
+            <JSONEditor height={this.state.viewHeight} ref={this.onJSONEditorReady} json={this.state.value} onValidate={this.validateJSON} />
+            <div style={{ textAlign: 'center', marginTop: '15px' }}>
+                <Space>
+                    <Button onClick={this.loadJSON} type='outline' icon={<IconImport />}>从文件加载</Button>
+                    <Button onClick={this.saveJSON} type='outline' icon={<IconSave />}>保存到本地</Button>
+                    <Button onClick={this.loadCacheJSON} type='outline' icon={<IconShrink />}>从缓存加载</Button>
+                    <Popconfirm
+                        title='请输入名字'
+                        content={
+                            <Input onChange={(value) => {
+                                this.setState({ tmpName: value })
+                            }} placeholder='请输入名字' />
+                        }
+                        onOk={this.saveJSON2Cache}
+                        onCancel={() => {
+                            this.setState({ tmpName: '' })
+                        }}
+
+                    >
+                        <Button type='outline' icon={<IconClockCircle />}>保存到缓存</Button>
+                    </Popconfirm>
+                    <Button onClick={this.clearJSON} type='outline' icon={<IconRefresh />}>清空输入</Button>
+                    <Button onClick={this.toGoStruct} type='outline' icon={<IconFire />}>转Go结构体</Button>
+                    <Button onClick={this.toString} type='outline' icon={<IconAlignCenter />}>序列化</Button>
+                </Space>
+            </div>
+            <Drawer
+                width={350}
+                title={<span>JSON缓存列表</span>}
+                visible={this.state.cacheVisible}
+                onCancel={() => {
+                    this.setState({ cacheVisible: false });
+                }}
+                footer={null}
+            >
+                <List
+                    dataSource={this.state.cacheNames}
+                    size='small'
+                    render={(item, index) => <List.Item key={index} extra={
+                        <Space size='mini'>
+                            <Button onClick={this.importCacheJSON.bind(this, item)} type='text' size='mini'>导入</Button>
+                            <Button onClick={this.copyCacheJSON.bind(this, item)} type='text' size='mini'>复制</Button>
+                        </Space>
+                    }>{item}</List.Item>}
+                />
+            </Drawer>
+            <Modal
+                title={this.state.convertTitle}
+                alignCenter={false}
+                visible={this.state.visible}
+                footer={null}
+                style={{ width: '60%', top: '100' }}
+                autoFocus={false}
+                focusLock={true}
+                onCancel={() => {
+                    this.setState({ visible: false });
+                }}
+            >
+                <ClickToCopy value={this.state.convert}>复制</ClickToCopy>
+                <pre>{this.state.convert}</pre>
+            </Modal>
+        </div>
+
+    }
 }
+
+export default App
--- src/config/menu.js
+++ src/config/menu.js
@@ -1,7 +1,6 @@
 import { IconHome, IconCodeSquare, IconScan, IconLiveBroadcast, IconHighlight, IconCodeBlock, IconQrcode } from '@arco-design/web-react/icon';
 export default [
-    { 'key': '/', 'icon': <IconHome />, 'title': '主页' },
-    { 'key': '/json/create', 'icon': <IconCodeBlock />, 'title': 'JSON' },
+    { 'key': '/', 'icon': <IconHome />, 'title': 'JSON' },
     { 'key': '/web/tool', 'icon': <IconHighlight />, 'title': 'Web工具' },
     { 'key': '/http/web_static', 'icon': <IconLiveBroadcast />, 'title': '静态资源服务器' },
 ]
