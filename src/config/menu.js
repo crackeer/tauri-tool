1fe03bf0c31a8d8450f4e5a98de81fba485d1f22
liuhu016 <liuhu016@realsee.com>
2024-03-27 12:08:12 +0800

    update

--- src/config/menu.js
+++ src/config/menu.js
@@ -1,9 +1,16 @@
-import { IconHome, IconCodeSquare, IconScan, IconLiveBroadcast, IconHighlight, IconCodeBlock, IconQrcode, IconList } from '@arco-design/web-react/icon';
+import {
+  IconHome,
+  IconCodeSquare,
+  IconScan,
+  IconLiveBroadcast,
+  IconHighlight,
+  IconCodeBlock,
+  IconQrcode,
+  IconList,
+} from "@arco-design/web-react/icon";
 export default [
-    { 'key': '/', 'icon': <IconHome />, 'title': '主页' },
-    { 'key': '/json/create', 'icon': <IconCodeBlock />, 'title': 'JSON' },
-    { 'key': '/memo', 'icon': <IconList />, 'title': 'Memo' },
-    { 'key': '/web/tool', 'icon': <IconHighlight />, 'title': 'Web工具' },
-    { 'key': '/http/web_static', 'icon': <IconLiveBroadcast />, 'title': 'SimpleHttp' },
-
-]
+  { key: "/", icon: <IconHome />, title: "JSON" },
+  { key: "/memo", icon: <IconList />, title: "Memo" },
+  { key: "/web/tool", icon: <IconHighlight />, title: "Web工具" },
+  { key: "/http/web_static", icon: <IconLiveBroadcast />, title: "SimpleHttp" },
+];
